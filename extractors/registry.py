e003efcb44468ab578c95a9dc10ba4029e597822
itonecart <saumitra.shanker@gmail.com>
2026-05-24 19:30:21 +0530

    Register SMAP extractor

--- extractors/registry.py
+++ extractors/registry.py
@@ -1,24 +1,72 @@
-# Extractor registry foundation
+# Extractor registry
+
+from extractors.smap_extractor import SMAPExtractor
+
 
 class ExtractorRegistry:
 
-    def __init__(self):
+    def __init__(
+
+        self,
+
+        earthdata,
+
+        settings
+
+    ):
 
         self.extractors = {}
 
+        self.register(
+
+            "smap",
+
+            SMAPExtractor(
+
+                earthdata,
+
+                settings
+
+            )
+
+        )
+
     def register(
+
         self,
+
         name,
+
         extractor
+
     ):
 
-        self.extractors[name] = extractor
+        self.extractors[
+            name
+        ] = extractor
 
     def get(
+
         self,
+
         name
+
     ):
 
         return self.extractors.get(
+
             name
+
+        )
+
+    def available(
+
+        self
+
+    ):
+
+        return list(
+
+            self.extractors.keys()
+
         )
