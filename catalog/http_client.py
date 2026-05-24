a1d81746ea179b1de96ef898ab612b874eeca1f8
itonecart <saumitra.shanker@gmail.com>
2026-05-24 19:23:39 +0530

    Implement HTTP transport layer

--- catalog/http_client.py
+++ catalog/http_client.py
@@ -1,28 +1,90 @@
-# HTTP foundation
-
 import aiohttp
 
 
 class HttpClient:
 
-    def __init__(self):
+    def __init__(
 
-        self.timeout = 60
+        self,
+
+        timeout=60
+
+    ):
+
+        self.timeout = timeout
 
     async def get(
+
         self,
+
         url,
+
         headers=None,
+
         params=None
+
     ):
 
-        raise NotImplementedError
+        timeout = aiohttp.ClientTimeout(
+
+            total=self.timeout
+
+        )
+
+        async with aiohttp.ClientSession(
+
+            timeout=timeout
+
+        ) as session:
+
+            async with session.get(
+
+                url,
+
+                headers=headers,
+
+                params=params
+
+            ) as response:
+
+                response.raise_for_status()
+
+                return await response.json()
 
     async def post(
+
         self,
+
         url,
+
         payload=None,
+
         headers=None
+
     ):
 
-        raise NotImplementedError
+        timeout = aiohttp.ClientTimeout(
+
+            total=self.timeout
+
+        )
+
+        async with aiohttp.ClientSession(
+
+            timeout=timeout
+
+        ) as session:
+
+            async with session.post(
+
+                url,
+
+                headers=headers,
+
+                json=payload
+
+            ) as response:
+
+                response.raise_for_status()
+
+                return await response.json()
