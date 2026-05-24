3925bb26d79f56b8fae45966e3feb9f42c5a787f
itonecart <saumitra.shanker@gmail.com>
2026-05-24 19:01:35 +0530

    Add parcel geometry foundation

---
+++ catalog/parcel_geometry.py
@@ -0,0 +1,15 @@
+# Parcel geometry foundation
+
+class ParcelGeometry:
+
+    def __init__(self):
+
+        pass
+
+    async def build(
+        self,
+        lat,
+        lng
+    ):
+
+        raise NotImplementedError
