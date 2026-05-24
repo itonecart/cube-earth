59d2447d62bb93df1f250be179bf195afe3951d9
itonecart <saumitra.shanker@gmail.com>
2026-05-24 19:14:33 +0530

    Implement Earthdata catalog interface

--- catalog/earthdata_client.py
+++ catalog/earthdata_client.py
@@ -1,7 +1,37 @@
 # Earthdata catalog client
 
+from catalog.auth import EarthdataAuth
+
+
 class EarthdataClient:
 
-    def __init__(self):
+    def __init__(
+        self,
+        auth: EarthdataAuth
+    ):
 
-        pass
+        self.auth = auth
+
+    async def collections(
+        self,
+        keyword=None
+    ):
+
+        raise NotImplementedError
+
+    async def granules(
+        self,
+        collection,
+        bbox,
+        start_date,
+        end_date
+    ):
+
+        raise NotImplementedError
+
+    async def metadata(
+        self,
+        granule_id
+    ):
+
+        raise NotImplementedError
